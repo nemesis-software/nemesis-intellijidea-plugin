d7d6137e963d2e536f267e8a265cb2f64ecba008
petar.tahchiev <paranoiabla@gmail.com>
2018-06-24 13:24:59 +0300

    Rename intellijdea to intellijidea.

--- src/com/nemesis/intellijidea/plugin/NemesisIcons.java
+++ src/com/nemesis/intellijidea/plugin/NemesisIcons.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.nemesis.intellijdea.plugin;
+package com.nemesis.intellijidea.plugin;
 
 import com.intellij.openapi.util.IconLoader;
 
